f8f8ed38b6ae6e9b79f1939e60bf37d822c6c2a1
Michael Parker <michael@parker.gg>
2020-05-28 17:15:27 -0500

    fix flaky coverage for js

--- src/client/test/components/ArticleList.spec.js
+++ src/client/test/components/ArticleList.spec.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import {Provider} from 'react-redux';
-import {ConnectedRouter} from 'connected-react-router';
-import {render} from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Chance from 'chance';
+import React from "react";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Chance from "chance";
 
-import { store, history} from '../../store';
-import ArticleList from '../../components/ArticleList';
+import { store, history } from "../../store";
+import ArticleList from "../../components/ArticleList";
 
 const chance = new Chance();
 
-const createRandomArticle = () => ({
+const createRandomArticle = (favorited = chance.bool()) => ({
     slug: chance.guid(),
-    favorited: chance.bool(),
+    favorited,
     author: {
         username: chance.word(),
-        image: chance.url(),
+        image: chance.url()
     },
     createdAt: chance.date(),
     favoritesCount: chance.d6(),
@@ -24,26 +24,36 @@ const createRandomArticle = () => ({
     tagList: chance.n(chance.word, chance.d6())
 });
 
-test('renders a loading div when no articles are loaded', () => {
-    const {queryAllByText} = render(<ArticleList/>);
-    const query = queryAllByText('Loading...')
+describe("article list", () => {
+    let randomArticles;
 
-    expect(query).toHaveLength(1);
-});
+    beforeEach(() => {
+        randomArticles = chance.shuffle([
+            createRandomArticle(true),
+            createRandomArticle(false),
+            ...chance.n(createRandomArticle, chance.d4())
+        ]);
+    });
 
-test('renders each article list', () => {
-    const numberOfArticles = chance.d6();
-    const articles = chance.n(createRandomArticle, numberOfArticles)
+    test("renders a loading div when no articles are loaded", () => {
+        const { queryAllByText } = render(<ArticleList/>);
+        const query = queryAllByText("Loading...");
 
-    const {queryAllByText} = render(
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <ArticleList articles={articles}/>
-            </ConnectedRouter>
-        </Provider>
-    );
+        expect(query).toHaveLength(1);
+    });
 
-    articles.forEach((article) => {
-        expect(queryAllByText(article.title)).toHaveLength(1);
+    test("renders each article list", () => {
+        const { queryAllByText } = render(
+            <Provider store={store}>
+                <ConnectedRouter history={history}>
+                    <ArticleList articles={randomArticles}/>
+                </ConnectedRouter>
+            </Provider>
+        );
+
+        randomArticles.forEach((article) => {
+            expect(queryAllByText(article.title)).toHaveLength(1);
+        });
     });
 });
+
