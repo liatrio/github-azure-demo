37402b3bb690e917831fa6230f3822039c7d03eb
Takumi Nishida <takumin@liatrio.com>
2020-06-02 10:09:38 -0700

    test terraform

--- terraform/application/variables.tf
+++ terraform/application/variables.tf
@@ -22,4 +22,8 @@ variable "service_app_scm_type" {
   default = "LocalGit"
 }
 
-variable "client_secret" {}
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+
--- terraform/firewall_rules/variables.tf
+++ terraform/firewall_rules/variables.tf
@@ -14,4 +14,8 @@ variable "sql_firewall_ip_addresses" {
   type = set(string)
 }
 
-variable "client_secret" {}
+variable "ARM_SUBSCRIPTION_ID" {}
+variable "ARM_CLIENT_ID" {}
+variable "ARM_CLIENT_SECRET" {}
+variable "ARM_TENANT_ID" {}
+
