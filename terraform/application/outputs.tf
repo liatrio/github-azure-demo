c604afb3e59b43712ea66934f39498dc72e36656
Takumi Nishida <takumin@liatrio.com>
2020-06-02 14:00:59 -0700

    testing terraform outputs

--- terraform/application/outputs.tf
+++ terraform/application/outputs.tf
@@ -9,3 +9,7 @@ output "sql_server_name" {
 output "backend_app_service_ip_addresses" {
   value = split(",", azurerm_app_service.app_service.possible_outbound_ip_addresses)
 }
+
+output "azure_app_hostname" {
+  value = azurerm_app_service.default_site_hostname
+}
