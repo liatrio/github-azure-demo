193d8bdbf03b1d7a8819bdeea92e9c2fe7b61fc8
Sean Jordan <jordansean10@gmail.com>
2020-06-03 08:55:02 -0700

    still need to add service principal

--- terraform-bootstrap/service_principal.tf
+++ terraform-bootstrap/service_principal.tf
@@ -1,6 +1,10 @@
 data "azurerm_subscription" "primary" {
 }
 
+resource "azuread_service_principal" "github_action" {
+    # TODO
+}
+
 resource "azurerm_role_definition" "github_action_terraform_role" {
   name        = "GithubActionTerraformRole"
   scope       = data.azurerm_subscription.primary.id
@@ -32,3 +36,11 @@ resource "azurerm_role_definition" "github_action_terraform_role" {
     data.azurerm_subscription.primary.id, 
   ]
 }
+
+resource "azurerm_role_assignment" "github_action_terraform_role_assignment" {
+  scope              = data.azurerm_subscription.primary.id
+  role_definition_id = azurerm_role_definition.github_action_terraform_role.id
+  principal_id       = data.azurerm_client_config.github_action.object_id
+  depends_on         = [azurerm_client_config.github_action]
+}
+
