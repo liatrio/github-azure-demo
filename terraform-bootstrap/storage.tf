8b09d93bdc30a7429e882e849f13153ef85e6ef5
Sean Jordan <seanj@liatrio.com>
2020-06-05 09:38:30 -0700

    Added comment-on-terraform-plan action and added terraform apply step to deploy action (#60)
    * Added action to run terraform plan on PR
    
    * Removed env, streamlined terraform
    
    * Added extra steps back in
    
    * ran terraform fmt
    
    * Added code to comment on terraform plan
    
    * added correct branch
    
    * Added debug
    
    * Added extra terraform plan just for fun:
    
    * Added synchronize
    
    * Removed trace
    
    * Added parenthesis
    
    * Added operator
    
    * removed statement
    
    * test
    
    * throwing out error code from grep
    
    * removing dash
    
    * Added deployment terraform apply action, removed synchronize
    
    * Added terraform step to deploy action
    
    Co-authored-by: Takumi Nishida <takumin@liatrio.com>

--- terraform-bootstrap/storage.tf
+++ terraform-bootstrap/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name     = "githubworkshop"
+  name                     = "githubworkshop"
   resource_group_name      = azurerm_resource_group.github_workshop.name
   location                 = azurerm_resource_group.github_workshop.location
   account_tier             = "Standard"
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  name     = "tfstate"
+  name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
